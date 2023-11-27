73970edd5374c1aed812063c4afe2b066b57e1ee
Sthuthi <sthuthiiiii135@gmail.com>
2023-11-27 19:18:26 +0530

    polyadd

--- llpolyadd.c
+++ llpolyadd.c
@@ -27,47 +27,70 @@ void read(struct node **poly,int coeff,int expo)
 		ptr=ptr->next;
 	ptr->next=new;
 }
-void add(struct node **poly1,struct node **poly2,struct node **sum)
+void add(struct node **poly1, struct node **poly2)
 {
-	struct node *ptr1=*poly1;
-	struct node *ptr2=*poly2;
-	struct node *ptrR=*sum;
-	while(ptr1!=NULL && ptr2!=NULL)
-	{
-		if(ptr1->expo==ptr2->expo)
-		{
-			ptrR->coeff=ptr1->coeff+ptr2->coeff;
-			ptrR->expo=ptr1->expo;
-			ptr1=ptr1->next;
-			ptr2=ptr2->next;
-		}
-		else if(ptr1->expo>ptr2->expo)
-		{
-			ptrR->coeff=ptr1->coeff;
-			ptrR->expo=ptr1->expo;
-			ptr1=ptr1->next;
-		}
-		else
-		{
-			ptrR->coeff=ptr2->coeff;
-			ptrR->expo=ptr2->expo;
-			ptr2=ptr2->next;
-		}
-	}
-	while(ptr1!=NULL)
-	{
-		ptrR->coeff=ptr1->coeff;
-		ptrR->expo=ptr1->expo;
-		ptr1=ptr1->next;
-	}
-	while(ptr2!=NULL)
-	{
-		ptrR->coeff=ptr2->coeff;
-		ptrR->expo=ptr2->expo;
-		ptr2=ptr2->next;
+    struct node *ptr1 = *poly1;
+    struct node *ptr2 = *poly2;
+    struct node *resultHead = NULL;
+    struct node *ptrR = NULL;
+
+    while (ptr1 != NULL || ptr2 != NULL)
+    {
+        struct node *newNode = (struct node *)malloc(sizeof(struct node));
+        newNode->next = NULL;
+
+        if (ptr1 != NULL && ptr2 != NULL)
+        {
+            if (ptr1->expo == ptr2->expo)
+            {
+                newNode->coeff = ptr1->coeff + ptr2->coeff;
+                newNode->expo = ptr1->expo;
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+            }
+            else if (ptr1->expo > ptr2->expo)
+            {
+                newNode->coeff = ptr1->coeff;
+                newNode->expo = ptr1->expo;
+                ptr1 = ptr1->next;
+            }
+            else
+            {
+                newNode->coeff = ptr2->coeff;
+                newNode->expo = ptr2->expo;
+                ptr2 = ptr2->next;
+            }
+        }
+        else if (ptr1 != NULL)
+        {
+            newNode->coeff = ptr1->coeff;
+            newNode->expo = ptr1->expo;
+            ptr1 = ptr1->next;
+        }
+        else if (ptr2 != NULL)
+        {
+            newNode->coeff = ptr2->coeff;
+            newNode->expo = ptr2->expo;
+            ptr2 = ptr2->next;
+        }
+
+        // Update the result linked list
+        if (resultHead == NULL)
+        {
+            resultHead = newNode;
+            ptrR = resultHead;
+        }
+        else
+        {
+            ptrR->next = newNode;
+            ptrR = newNode;
+        }
+		result = resultHead;
+
 	}
 
 }
+
 void display(struct node **poly)
 {
 	if(*poly==NULL)
@@ -89,13 +112,14 @@ void main()
 	int coefficient,exponent;
 	int op;
 	printf("MENU:\n");
-	printf("1.INSERT INTO POLYNOMIAL 1\n2.INSERT INTO POLYNOMAL 2\n3.ADD THE POLYNOMIAL\n4.DISPLAY THE POLYNOMIAL\n5.exit");
+	printf("1.INSERT INTO POLYNOMIAL 1\n2.INSERT INTO POLYNOMAL 2\n3.ADD THE POLYNOMIAL\n4.DISPLAY THE POLYNOMIAL\n5.exit\n");
 	do{
 		printf("Enter your option:");
 		scanf("%d",&op);
 		switch(op)
 		{
 			case 1:
+				printf("reading into polynomial 1\n");
 				printf("Enter the coefficient:");
 				scanf("%d",&coefficient);
 				printf("Enter the exponent:");
@@ -103,6 +127,7 @@ void main()
 				read(&p1,coefficient,exponent);
 				break;
 			case 2:
+				printf("reading into polynomial 2\n");
 				printf("Enter the coefficient:");
 				scanf("%d",&coefficient);
 				printf("Enter the exponent:");
@@ -110,7 +135,7 @@ void main()
 				read(&p2,coefficient,exponent);
 				break;
 			case 3:
-				add(&p1,&p2,&result);
+				add(&p1,&p2);
 				break;
 			case 4:
 				printf("FIRST POLYNOMIAL:\n");
