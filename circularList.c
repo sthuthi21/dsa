9edca538683362ec2306ffd41217dc43ff9b3393
Sthuthi <sthuthiiiii135@gmail.com>
2023-12-06 21:54:55 +0530

    circularlist

--- circularList.c
+++ circularList.c
@@ -23,30 +23,92 @@ struct node* create_node()
     return new;
 }
 
-void insertList()
+void insertFront()
 {
 	struct node *new=create_node();
-	if(new==NULL)
+	if(head==NULL)
 	{
-		printf("memory insufficient\n");
+		head=new;
+		head->next=new;
+		head->prev=new;
 	}
 	else
 	{
-		if(head==NULL)
+		new->next=head;
+		new->prev=head->prev;
+		head->prev->next=new;
+		head=new;          
+	}
+}
+void insertLast()
+{
+	struct node *new=create_node();
+	if(head==NULL)
+	{
+		head=new;
+		head->next=new;
+		head->prev=new;
+	}
+	else
+	{
+		new->prev=head->prev;
+		head->prev->next=new;
+		new->next=head;
+		head->prev=new;
+	}
+
+}
+
+void deleteFirst()
+{
+	if(head==NULL)
+	{
+		printf("No list exists.\n");
+		return;
+	}
+	else
+	{
+		struct node *ptr=head;
+		if(ptr->next==head)
 		{
-			head=new;
-			head->next=new;
-			head->prev=new;
+			head=NULL;
 		}
 		else
 		{
-			struct node *ptr=head;
-			while(ptr->next!=head)
-				ptr=ptr->next;
-			new->next=head;
-			ptr->next=new;           
+			head->next->prev=head->prev;
+			head->prev->next=head->next;
+			head=head->next;
 		}
+		printf("the deleted element:%d",ptr->data);
+		free(ptr);
 	}
+	
+	
+}
+
+void deleteLast()
+{
+	if(head==NULL)
+	{
+		printf("no linked list exists:\n");
+		return;
+	}
+	else
+	{
+		struct node *ptr=head->prev;
+		if(head->prev==head)
+		{
+			head=NULL;
+		}
+		else
+		{
+			head->prev=head->prev->prev;
+			head->prev->next=head;
+		}
+		printf("the deleted element:%d",ptr->data);
+		free(ptr);
+	}
+	
 }
 
 void display()
@@ -71,9 +133,32 @@ void display()
 
 void main()
 {
-	insertList();
-	insertList();
-	insertList();
-	insertList();
-	display();
+    
+    int op,item,position;
+    printf("MENU");
+	printf("WHAT OPERATIONS DO  YOU WANT TO DO?\n");
+	printf("1.insertion at begin\n2.insert at end\n3.delete from front\n4.delete from end\n5.display");
+    printf("press any other number to exit\n");
+	do{
+		printf("enter your option:");
+		scanf("%d",&op);
+		switch(op)
+		{
+			case 1:
+				insertFront();
+				break;
+			case 2:
+				insertLast();
+				break;
+			case 3:
+				deleteFirst();
+				break;
+			case 4:
+				deleteLast();
+				break;
+			case 5:
+                display();
+                break;
+		}
+	}while(op<6);
 }
